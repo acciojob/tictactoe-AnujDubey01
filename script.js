7a27f837649a479427d26a6fcdb648b81bb77a34
Anuj Dubey <149352591+AnujDubey01@users.noreply.github.com>
2025-05-13 15:34:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Get DOM elements
-cy.get('#1').click({ force: true });
 const player1Input = document.querySelector("#player1");
 const player2Input = document.querySelector("#player2");
 const board = document.querySelector(".board");
